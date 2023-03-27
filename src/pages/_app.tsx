e0d2372e0f45294fc0268c0dd51f0aae63284930
Park Kyung Hyun <pikadev1771@gmail.com>
2023-03-27 23:13:17 +0900

    next/font 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,10 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import {
-  JetBrains_Mono,
-  Noto_Sans_KR,
-  Montserrat,
-  Poppins,
-} from '@next/font/google';
-
-export const cls = (...classnames: string[]) => {
-  return classnames.join(' ');
-};
+import { JetBrains_Mono } from '@next/font/google';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main
-      className={cls(
-        jetBrains_Mono.className,
-        notoSansKr.className,
-        poppins.variable,
-        montserrat.variable
-      )}
-    >
+    <main className={jetBrains_Mono.className}>
       <Component {...pageProps} />
     </main>
   );
@@ -36,19 +20,4 @@ const jetBrains_Mono = JetBrains_Mono({
     'Roboto',
     'sans-serif',
   ],
-});
-
-const notoSansKr = Noto_Sans_KR({
-  weight: ['100', '300', '400', '500', '700', '900'],
-  display: 'swap',
-});
-const montserrat = Montserrat({
-  weight: ['500', '700'],
-  display: 'swap',
-  variable: '--montserrat',
-});
-const poppins = Poppins({
-  weight: ['600', '700'],
-  display: 'swap',
-  variable: '--poppins',
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { Noto_Sans_KR } from '@next/font/google';
+import styles from '../styles/Home.module.css';
+
+const notoSansKr = Noto_Sans_KR({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--notosanskr',
+});
 
 export default function Home() {
   return (
@@ -9,6 +16,9 @@ export default function Home() {
       </Head>
       <div>
         <h1>Sentiment Analysis Diary</h1>
+        <div className={notoSansKr.variable}>
+          <h2 className={styles.text}>야호</h2>
+        </div>
       </div>
     </>
   );
