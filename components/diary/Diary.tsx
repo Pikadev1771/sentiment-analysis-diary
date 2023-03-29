c794caf123e466aeb23740f5c7f7e384a3c0d785
Park Kyung Hyun <pikadev1771@gmail.com>
2023-03-29 22:37:52 +0900

    feat - #5 react-calendar 생성

--- components/diary/Diary.tsx
+++ components/diary/Diary.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import styles from '../../styles/Diary.module.css';
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKr = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--notosanskr',
+});
+
 export default function Diary() {
   return (
     <div>
-      <DiaryContainer></DiaryContainer>
+      <DiaryContainer>
+        <Container>
+          <h1>Today&apos;s Diary</h1>
+          <div className={notoSansKr.variable}>
+            <p className={styles.text}>야호</p>
+          </div>
+        </Container>
+      </DiaryContainer>
     </div>
   );
 }
@@ -13,4 +29,12 @@ const DiaryContainer = styled.div`
   width: 50vw;
   height: 90vh;
   border: 1px solid blue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 80%;
+  border: 1px solid green;
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,7 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Noto_Sans_KR } from '@next/font/google';
-import styles from '../styles/Home.module.css';
 import styled from 'styled-components';
 import ReactCalendar from '../components/diary/ReactCalendar';
 import Diary from '../components/diary/Diary';
-
-const notoSansKr = Noto_Sans_KR({
-  weight: ['100', '300', '400', '500', '700', '900'],
-  variable: '--notosanskr',
-});
 
 export default function Home() {
   return (
@@ -18,13 +10,8 @@ export default function Home() {
         <title>Sentiment Analysis Diary</title>
       </Head>
       <div>
-        {/* <h1>Sentiment Analysis Diary</h1>
-        <div className={notoSansKr.variable}>
-          <p className={styles.text}>야호</p>
-        </div> */}
         <MainContainer>
           <ReactCalendar />
-
           <Diary />
         </MainContainer>
       </div>
@@ -33,7 +20,6 @@ export default function Home() {
 }
 const MainContainer = styled.div`
   display: flex;
-  /* justify-content: space-around; */
   width: 100%;
   border: 1px solid green;
 `;
